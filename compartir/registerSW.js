017bea40ed6f746616ee69f86e2a6a7596381bd0
mentactitud <mentactitud@users.noreply.github.com>
2026-07-12 18:42:58 +0000

    deploy: 4cfbd5aa8fc67699d2ad42fbad56b913c1e79ad3

---
+++ compartir/registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/Tour-de-France/compartir/sw.js', { scope: '/Tour-de-France/compartir/' })})}
